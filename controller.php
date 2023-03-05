55639d90cc900f242035471040a60436cce15181
TudorPal <116271480+TudorPal@users.noreply.github.com>
2023-03-05 21:06:03 +0200

    Add files via upload

---
+++ controller.php
@@ -0,0 +1,113 @@
+<?php
+session_start();
+
+if (empty($_SESSION['company_id'])) {
+	header("Location: index.php");
+}
+
+$host = 'localhost';
+$username = 'bestcjro_adelin';
+$password = 'adelin1';
+$dbname = 'bestcj_standuri';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if (isset($_POST['id'])) {
+	$company_id = $_SESSION['company_id'];
+	$table_id = $_POST['id'];
+
+	if (verifySize($company_id, $table_id) == 0) {
+		echo "notOk";
+		die;
+	}
+
+	occupyTable($table_id, $company_id);
+}
+
+if (isset($_POST['init'])) {
+	$company_id = $_SESSION['company_id'];
+	$company_size = $_SESSION['company_size'];
+
+	global $conn;
+	$sql = 'SELECT id FROM `tables` WHERE size="' . $company_size . '" AND id NOT IN (SELECT table_id FROM `companies_tables`)';
+	$result = $conn->query($sql);
+
+	$rows = array();
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			$rows[] = $row;
+		}
+	}
+	$conn->close();
+
+	die(json_encode(array('available' => $rows)));
+
+}
+
+if (isset($_POST['update'])) {
+
+	global $conn;
+	$sql = 'SELECT table_id AS t, company_id AS c FROM `companies_tables`';
+	$result = $conn->query($sql);
+
+	$rows = array();
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			$rows[] = $row;
+		}
+	}
+	$conn->close();
+
+	die(json_encode($rows));
+}
+
+
+/**
+ * Verify if the selected table is the same size as the company is allowed
+ * @param $company_id
+ * @param $table_id
+ * @return int
+ */
+function verifySize($company_id, $table_id) {
+
+	global $conn;
+	$sql = 'SELECT COUNT(1) FROM `companies` JOIN `tables` ON `companies`.size = `tables`.size WHERE `tables`.id=' . $table_id . ' AND `companies`.id=' . $company_id;
+	$result = $conn->query($sql);
+
+	return $result->num_rows ? 1 : 0;
+}
+
+/**
+ * Occupy a table with a company
+ * @param $table_id
+ * @param $company_id
+ */
+function occupyTable($table_id, $company_id) {
+	if (!verifyTime()) {
+		die;
+	}
+
+	global $conn;
+	$sql = "INSERT INTO `companies_tables` (`company_id`, `table_id`) VALUES (" . $company_id . ", " . $table_id . ");";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "Table selected successfully!";
+	} else {
+		echo "A table has already been selected!";
+	}
+	$conn->close();
+}
+
+function verifyTime() {
+
+	$open_date = '05.03.2023, 10:00 AM GMT+2';
+	$open_time = (int)strtotime($open_date);
+
+	$now = time();
+
+	if ($now < $open_time) {
+		echo 'Tool not open yet. Please wait until: ' . $open_date;
+	}
+
+	return $now > $open_time;
+}
---
+++ map_old.php
@@ -0,0 +1,282 @@
+<?php {
+	session_start();
+	if (empty($_SESSION['company_id'])) {
+		header("Location: index.php");
+	}
+} ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>JobShop | Standuri</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+</head>
+<body>
+
+<div id="header">
+    <img style="width: 420px" src="logo.png">
+
+</div>
+
+    <div id="sala"><center><br><br>SALA</center></div>
+    
+    
+<div id="tables">
+
+    <div>
+        <div class="bl br" style="width: 1100px;">
+            <!-- TOP ROOM -->
+            <div class="bt bb" style="padding-top:4px;">
+
+                <div>
+
+                    <div class="inline"> <!-- TOP FIRSTROW -->
+                        <div class="empty" style="width: 67.5px;"></div>
+                        <div data-table-id="1" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="2" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="3" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="" class="table hts4 blocked"> </div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="4" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="5" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="6" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="7" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="8" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        
+                        <div class="empty" style="width: 63.25px;"></div>
+                    </div>
+                        <div class="empty block" style="height: 45px; width:692px;"></div>
+                    
+                    <div class="inline"> <!-- TOP SECONDROW -->
+                        <div class="empty" style="width: 123.5px;"></div>
+                       
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="9" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="" class="table hts4 blocked" style="background-color:red;"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="10" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="" class="table hts4 blocked" style="background-color:blue;"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="11" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="" class="table hts4 blocked" style="background-color:red;"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="12" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="13" class="table hts4"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div class="empty"></div>
+                        <div data-table-id="14" class="table vts4"></div>
+                        <div class="empty" style="width: 63.25px;"></div>
+                    </div>
+                    
+                </div>
+            
+
+                    <div class="inline"> <!-- CENTER -->
+
+                        <div class="inline"> <!-- CENTER LEFT -->
+
+                            <div class="empty block" style="height: 25px; width:692px;"></div>
+
+
+                            <div class="inline">
+                                <div class="empty" style="width: 10px;"></div>
+                                <div data-table-id="15" class="table vts4"></div>
+                            <div class="empty" style="width: 35px;"></div>
+                            <div data-table-id="16" class="table vts4"></div>
+                            <div class="empty"></div>     
+                                
+                            </div>
+    
+                        </div>
+                        
+                        
+                         <div class="inline"> <!-- CENTER LEFT -->
+
+                            <div class="empty block" style="height: 25px; width:692px;"></div>
+
+
+                            <div class="inline">
+                                <div class="empty" style="width: 10px;"></div>
+                                
+                            <div class="empty" style="width: 68px;"></div>
+                            <div data-table-id="17" class="table vts4"></div>
+                            <div class="empty"></div>     
+                                
+                            </div>
+    
+                        </div>
+                        
+                        <div class="inline"> <!-- CENTER LEFT -->
+
+                            <div class="empty block" style="height: 25px; width:692px;"></div>
+
+
+                            <div class="inline">
+                                <div class="empty" style="width: 10px;"></div>
+                                
+                            <div class="empty" style="width: 68px;"></div>
+                            <div data-table-id="" class="table vts4 blocked" style="background-color:blue;"></div>
+                            <div class="empty"></div>     
+                                
+                            </div>
+    
+                        </div>
+                        
+                        <div class="inline"> <!-- CENTER LEFT -->
+
+                            <div class="empty block" style="height: 25px; width:692px;"></div>
+
+
+                            <div class="inline">
+                                <div class="empty" style="width: 10px;"></div>
+                                
+                            <div class="empty" style="width: 68px;"></div>
+                            <div data-table-id= "18" class="table vts4"></div>
+                            <div class="empty"></div>     
+                                
+                            </div>
+    
+                        </div>
+                        
+                        
+                        <div class="inline"> <!-- CENTER LEFT -->
+
+                            <div class="empty block" style="height: 25px; width:692px;"></div>
+
+
+                            <div class="inline">
+                                <div class="empty" style="width: 10px;"></div>
+                                
+                            <div class="empty" style="width: 68px;"></div>
+                            <div data-table-id="19" class="table vts4" ></div>
+                                
+                            <div class="empty"></div>     
+                                
+                            </div>
+    
+                        </div>
+                        
+                        
+                        
+                        
+                        
+                        
+                        <div>
+
+                    <div class="inline"> <!-- TOP FIRSTROW -->
+                        <div style="width: 1px; height: 70px"></div>
+                        <div data-table-id="" class="table hts4" style="background-color:black; height:70px; width:140px; color:white; font-size:20px;"><center><br>INTRARE</center></div>
+                        
+                      
+                    </div>
+                    
+                </div>
+                        <div class="inline"> <!-- TOP FIRSTROW -->
+                        <div class="empty" style="width: 10px; height: 70px"></div>
+                        <div data-table-id="" class="table hts4" style="background-color:purple; color:white;"><center>BEST</center></div>
+                        
+                      
+                    </div>
+                        
+                        
+                       
+                        
+                        
+                        
+                        <div class="empty block" style="height: 120px; width:692px;"></div>
+                        
+                        
+                   
+
+                   
+
+
+                </div>
+
+            </div>
+        </div>
+
+</div>
+</div>
+
+<div style="width: 802px; margin: 10px auto">
+    <h2>Instrucțiuni</h2>
+    <p><b>Cu roșu sunt notate băile, cu negru scări iar cu albastru baruri.</b></p>
+    <p><b>Atenție! Dacă ați fost logat inainte de 10:00 a.m trebuie sa dați un hard refresh (CTRL+F5).</b></p>
+    <p>Aplicația va fi funcțională Marti, 03.03.2020, ora 10:00 A.M.</p>
+    <p>Veți putea alege dintre standurile marcate cu verde (corespunzătoare numarului de metri pătrați al pachetului
+        ales).</p>
+    <p>Standul ales nu va putea fi schimbat ulterior!</p>
+    <p>Pentru orice problemă tehnică vă rugam sa ne contactați la: +40 758 849 097 - Păduraru Răzvan</p>
+</div>
+
+</body>
+</html>
